026b0a1ca46dbd74698a1bd63b5a133ebbc5af84
laonmiku <laonmiku@gmail.com>
2024-07-04 18:20:45 +0900

    list프레임미완

--- greenlog front-end/src/components/mall/ListPage.jsx
+++ greenlog front-end/src/components/mall/ListPage.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Card, Table,Row,Col } from 'react-bootstrap'
+import SlidePage from '../../common/useful/SlidePage'
 
 const ListPage = () => {
   return (
-    <div>ListPage</div>
+    <div className='justify-content-center text-center'>
+        <h1 className='my-5'>피망마켓</h1>
+        
+            <div>슬라이드자리만들기 </div>
+        
+        <Row>
+            <Col lg={12} md={12} xs={12}>
+                <div className='mt-3'>
+                    <Table className='justify-content-center'>
+                    <tbody className='border border-white'>
+                    <tr>
+                        <td>
+                        <Card>
+                            <Card.Body>바디뱅</Card.Body>
+                            <Card.Footer> 발을씻자</Card.Footer>
+                            </Card>
+                        </td>
+                        <td><Card><Card.Body>바디뱅</Card.Body><Card.Footer> 발을씻자</Card.Footer></Card></td>
+                        <td><Card><Card.Body>바디뱅</Card.Body><Card.Footer> 발을씻자</Card.Footer></Card></td>
+                        <td><Card><Card.Body>바디뱅</Card.Body><Card.Footer> 발을씻자</Card.Footer></Card></td>
+                    </tr>
+                    <tr >
+                        <td><Card><Card.Body>바디뱅</Card.Body><Card.Footer> 발을씻자</Card.Footer></Card></td>
+                        <td><Card><Card.Body>바디뱅</Card.Body><Card.Footer> 발을씻자</Card.Footer></Card></td>
+                        <td><Card><Card.Body>바디뱅</Card.Body><Card.Footer> 발을씻자</Card.Footer></Card></td>
+                        <td><Card><Card.Body>바디뱅</Card.Body><Card.Footer> 발을씻자</Card.Footer></Card></td>
+                    </tr>
+                    <tr >
+                        <td><Card><Card.Body>바디뱅</Card.Body><Card.Footer> 발을씻자</Card.Footer></Card></td>
+                        <td><Card><Card.Body>바디뱅</Card.Body><Card.Footer> 발을씻자</Card.Footer></Card></td>
+                        <td><Card><Card.Body>바디뱅</Card.Body><Card.Footer> 발을씻자</Card.Footer></Card></td>
+                        <td><Card><Card.Body>바디뱅</Card.Body><Card.Footer> 발을씻자</Card.Footer></Card></td>
+                    </tr>
+                    </tbody>
+                    </Table> 
+                </div>
+            </Col>
+         </Row>
+    </div>
   )
 }
 
--- greenlog front-end/src/routers/RouterPage.jsx
+++ greenlog front-end/src/routers/RouterPage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import MainPage from '../common/home/MainPage'
 import UserRouter from './UserRouter'
+import MallRouter from './MallRouter'
 
 const RouterPage = () => {
   return (
     <Routes>
         <Route path='/' element={<MainPage/>}/>
         <Route path='/user/*' element={<UserRouter/>}/>
+        <Route path='/mall/*' element={<MallRouter/>}/>
     </Routes>
   )
 }
