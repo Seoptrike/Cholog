a0871d0e2884ee6b198cceadcdcb68e38503db9d
SANGKYUN2 <gr001231@naver.com>
2024-07-11 18:24:57 +0900

    review_printfb & reply_back

--- greenlog front-end/src/components/reply/InsertPage.jsx
+++ greenlog front-end/src/components/reply/InsertPage.jsx
@@ -2,115 +2,49 @@ import React, { useEffect, useState } from 'react'
 import { Row, Col, Form, Button, FormControl, Dropdown, Card, InputGroup } from 'react-bootstrap'
 import Pagination from 'react-js-pagination';
 import { BsList, BsHandThumbsUp, BsHandThumbsDown, BsChevronDown, BsThreeDotsVertical, BsArrowReturnRight } from "react-icons/bs";
+import axios from 'axios';
 
-const InsertPage = () => {
-    const [page, setPage] = useState(1);
-    const [size, setSize] = useState(5);
-    const [count, setCount] = useState(0);
-    const [reply_contents, setReply_contents] = useState('');
-    const [sort, setSort] = useState('latest')
-    const [wordCount, setWordCount] = useState(0);
-    const [onCancel, setOnCancel] = useState(false);
-    const [showRep, setShowRep] = useState(false);
-    const wordlimit = 1000;
+const InsertPage = ({bbs_key}) => {
+    const [form, setForm] = useState({
+        reply_bbs_key : bbs_key,
+        reply_writer : sessionStorage.getItem('uid'),
+        reply_contents : '',
+        reply_reaction : ''
+    });
 
-    const callAPI = async () => {
+    const {reply_bbs_key, reply_writer, reply_contents, reply_reaction} = form;
 
+    const onChangeForm = (e) => { 
+        setForm({...form, [e.target.name] : e.target.value});
     }
 
-    useEffect(() => {
-        callAPI();
-    }, [page, sort]);
-
-    useEffect(() => {
-        setWordCount(reply_contents.length);
-    }, [reply_contents])
-
-    const onInsert = () => {
+    const onSubmit = async(e) => {
+        e.preventDefault();
         if (reply_contents === '') {
             alert("댓글 내용을 입력해주세요!")
             return;
         }
-        else if (wordCount > wordlimit) {
-            alert('허용되는 글자 수가 초과되었습니다.')
-        }
-        else {
+        await axios.post('/reply/insert', form)
             alert('댓글 등록');
-            setReply_contents('');
-            setOnCancel(false);
-            callAPI();
         }
-    }
-
-    const handleSelectSort = (eventKey) => {
-        setSort(eventKey);
-        setPage(1);
-    };
-
-    const onClickCancel = () => {
-        setReply_contents('');
-        setOnCancel(false);
-    }
-
-    const toggleRep = () => {
-        setShowRep(!showRep);
-        setOnCancel(false);
-    };
 
     return (
         <div className='mt-5'>
             <h5 className='mt-5 text-center'>게시글 인서트 페이지</h5>
-        <Row className='justify-content-center mt-3'>
-            <Col xs={8}>
-                <Card className='mt-3'>
-                    <Row className='justify-content-center mt-3'>
-                        <Col xs={12} className='text-end'>
-                            <Dropdown onSelect={handleSelectSort}>
-                                <Dropdown.Toggle variant="" id="dropdown-basic">
-                                    <BsList />
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item eventKey="latest">최신순</Dropdown.Item>
-                                    <Dropdown.Item eventKey="rating">평점순</Dropdown.Item>
-                                    <Dropdown.Item eventKey="oldest">오래된순</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                            <Row className='justify-content-center'>
-                                <Col xs={9}>
-                                    <Form>
-                                        <FormControl value={reply_contents} onChange={(e) => setReply_contents(e.target.value)}
-                                            onFocus={() => setOnCancel(true)} as='textarea' rows={5} placeholder='내용을 입력해주세요.' />
-                                        <InputGroup.Text className='justify-content-end'>
-                                            <span>{wordCount} / {wordlimit}</span>
-                                        </InputGroup.Text>
-                                        <div className='text-end mt-3'>
-                                            <Button onClick={onClickCancel} variant=''
-                                                size="sm" className='text-end me-2' type='cancel' disabled={!onCancel}>취소</Button>
-                                            <Button onClick={onInsert} variant=''
-                                                size="sm" className='text-end' type='submit' disabled={reply_contents === ''}>등록</Button>
-                                        </div>
-                                    </Form>
-                                    <hr />
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-
+            <Row className='justify-content-center mt-5'>
+                <Col xs={6}>
                     <div>
-                        {count > size &&
-                            <Pagination
-                                activePage={page}
-                                itemsCountPerPage={size}
-                                totalItemsCount={count}
-                                pageRangeDisplayed={5}
-                                prevPageText={"‹"}
-                                nextPageText={"›"}
-                                onChange={(e) => setPage(e)} />
-                        }
+                        <form onSubmit={onSubmit} >
+                        <div className='text-end mt-3'>
+                            <Button 
+                                variant='' size="sm" className='text-end me-2' type='reset'>취소</Button>
+                            <Button variant='' size="sm" className='text-end' type='submit'>등록</Button>
+                        </div>
+                        </form>    
                     </div>
-                </Card>
-            </Col>
-        </Row>
+                </Col>
+            </Row>
+            <hr />
         </div>
     )
 }
