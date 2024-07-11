9e7dea1d7125f5fc9f6ed7e0a7c02695c516d5dc
SANGKYUN2 <gr001231@naver.com>
2024-07-11 11:29:13 +0900

    review_crud_front_back

--- greenlog front-end/src/components/review/ReviewPage.jsx
+++ greenlog front-end/src/components/review/ReviewPage.jsx
@@ -7,20 +7,19 @@ import { Rating } from '@mui/material';
 import Spa from '@mui/icons-material/Spa';
 import axios from 'axios';
 import '../../common/useful/Paging.css'
-import { useParams } from 'react-router-dom';
 
-const ReviewPage = () => {
-    const { review_mall_key } = useParams;
+const ReviewPage = ({ mall_key }) => {
     const [list, setList] = useState([]);
     const [page, setPage] = useState(1);
-    const [size, setSize] = useState(12);
+    const [size, setSize] = useState(3);
     const [count, setCount] = useState(0);
     const [sort, setSort] = useState('latest');
+    const review_writer = sessionStorage.getItem('uid'); // 현재 사용자 ID
 
     const callAPI = async () => {
-        const res = await axios.get(`/review/plist/${review_mall_key}`);
-        console.log(res.data);
-        setList(res.data.documents);
+        const res = await axios.get(`/review/plist/${mall_key}?page=${page}&size=${size}&sort=${sort}`);
+        const data = res.data.documents.map(doc => doc && { ...doc, isEdit: false, text: doc.review_contents, num : doc.review_rating });
+        setList(data);
         setCount(res.data.total);
     };
 
@@ -33,12 +32,31 @@ const ReviewPage = () => {
         setPage(1);
     };
 
-    const onDelete = async(review_key) => {
-        if(!window.confirm(`${review_key}번 댓글을 삭제하실래요?`)) return;
-        const res = await axios.post(`/review/delete/${review_key}`);
-        if(res.data.result===1) {
-            callAPI();
-        }
+    //삭제하기
+    const onDelete = async (review_key) => {
+        if (!window.confirm(`${review_key}번 댓글을 삭제하실래요?`)) return;
+        await axios.post(`/review/delete/${review_key}`);
+        alert('댓글 삭제 완료!');
+        callAPI();
+    };
+
+    //수정하기
+    const onUpdate = (review_key) => {
+        const data = list.map(review => (review.review_key === review_key) ? { ...review, isEdit: true } : review);
+        setList(data);
+    };
+
+    //등록하기
+    const onSave = async(review) => {
+        await axios.post(`/review/update/${review.review_key}`, { review_contents: review.text }, { review_rating : review.num});
+        alert('댓글 수정 완료!');
+        callAPI();
+    }
+
+    //취소하기
+    const onCancel = async(review_key) => {
+        const data = list.map(review => (review.review_key === review_key) ? { ...review, isEdit: false } : review);
+        setList(data);
     }
 
     return (
@@ -49,40 +67,16 @@ const ReviewPage = () => {
                     {list.map(review => (
                         <Card key={review.review_key} className='my-3'>
                             <Card.Body>
-                                {review.review_lock === 1 ? (
+                                {review.review_lock === 1 ? 
                                     <div className='mb-2'>비밀 댓글입니다.</div>
-                                ) 
                                 : 
-                                (
                                 <Row className='align-items-center'>
                                     <Col xs="auto">
                                         <img src="http://via.placeholder.com/30x30" width="70" height="70" className='rounded-circle' alt="profile" />
+                                        <div className='mt-2'>{review.review_writer}</div>
                                     </Col>
                                     <Col className="ps-2">
                                         <Row className='align-items-center'>
-                                            <Col>{review.review_writer}</Col>
-                                            <Col>
-                                                {review.review_lock === 1 && (
-                                                    <Col xs="auto">
-                                                        <FiLock style={{ color: "green" }} />
-                                                    </Col>
-                                                )}
-                                            </Col>
-                                            <Col xs="auto">
-                                                <Dropdown align="end" onSelect={handleSelectSort}>
-                                                    <Dropdown.Toggle variant="" id="dropdown-basic">
-                                                        <BsThreeDotsVertical />
-                                                    </Dropdown.Toggle>
-                                                    <Dropdown.Menu>
-                                                        <Dropdown.Item eventKey="update">수정하기</Dropdown.Item>
-                                                        <Dropdown.Item onClick={()=>onDelete(review.review_key)}
-                                                            eventKey="delete">삭제하기</Dropdown.Item>
-                                                        <Dropdown.Item eventKey="warning">신고하기</Dropdown.Item>
-                                                    </Dropdown.Menu>
-                                                </Dropdown>
-                                            </Col>
-                                        </Row>
-                                        <Row className='align-items-center my-2'>
                                             <Col>
                                                 <Rating
                                                     name='point'
@@ -95,13 +89,39 @@ const ReviewPage = () => {
                                                     emptyIcon={<Spa />}
                                                 />
                                             </Col>
+                                            <Col xs="auto">
+                                                <Dropdown align="end">
+                                                    <Dropdown.Toggle variant="" id="dropdown-basic">
+                                                        <BsThreeDotsVertical />
+                                                    </Dropdown.Toggle>
+                                                    {review_writer === review.review_writer ?
+                                                        (!review.isEdit ?
+                                                        <Dropdown.Menu>
+                                                            <Dropdown.Item onClick={() => onUpdate(review.review_key)}
+                                                                eventKey="update">수정하기</Dropdown.Item>
+                                                            <Dropdown.Item onClick={() => onDelete(review.review_key)}
+                                                                eventKey="delete">삭제하기</Dropdown.Item>
+                                                        </Dropdown.Menu>
+                                                        :
+                                                        <Dropdown.Menu>
+                                                            <Dropdown.Item onClick={()=> onSave(review)}
+                                                                eventKey="save">등록</Dropdown.Item>
+                                                            <Dropdown.Item onClick={()=> onCancel(review.review_key)}
+                                                                eventKey="cancel">취소</Dropdown.Item>
+                                                        </Dropdown.Menu>)
+                                                    :
+                                                    <Dropdown.Menu>
+                                                        <Dropdown.Item eventKey="hide">댓글 숨기기</Dropdown.Item>
+                                                        <Dropdown.Item eventKey="warning">댓글 신고하기</Dropdown.Item>
+                                                    </Dropdown.Menu>
+                                                    }
+                                                </Dropdown>
+                                            </Col>
                                         </Row>
-                                        <Row>
-                                            <Col>{review.review_contents}</Col>
-                                        </Row>
+                            
                                     </Col>
                                 </Row>
-                                 )}
+                                }
                             </Card.Body>
                         </Card>
                     ))}
@@ -116,7 +136,8 @@ const ReviewPage = () => {
                 pageRangeDisplayed={5}
                 prevPageText={"‹"}
                 nextPageText={"›"}
-                onChange={ (e)=>setPage(e) }/>
+                onChange={(e) => setPage(e)}
+            />
             }
         </div>
     )
